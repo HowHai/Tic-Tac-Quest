7e234ac9116b9a6abcd2f360f165aa1dfa12af11
Hai Nguyen <hmn91733@gmail.com>
2014-01-15 10:38:00 -0800

    Add gameOver

--- maincontroller.js
+++ maincontroller.js
@@ -2,6 +2,8 @@ app.controller("MainController", function($scope){
   var playerOneMoves = [];
   var playerTwoMoves = [];
   var gameBoard = [];
+  var gameOver = false;
+  $scope.statusBox = "Click start to play!";
   $scope.rowOne = [0,1,2];
   $scope.rowTwo = [3,4,5];
   $scope.rowThree = [6,7,8];
@@ -10,38 +12,53 @@ app.controller("MainController", function($scope){
                           [0,1,2], [3,4,5], [6,7,8],
                           [0,3,6], [1,4,7], [2,5,8],
                           [0,4,8], [2,4,6]
-                        ];
+                     ];
+
+  function displayStatus(status){
+    $scope.statusBox = status;
+  }
 
   $scope.selectedDiv = function(selected){
     var getDiv = $("#" + selected);
     var occupiedTerritory = isNaN(getDiv.html());
     var XorO = gameBoard.length % 2 == 0 ? "X" : "O";
 
-    if (XorO == 'X' && !occupiedTerritory)
+    if (XorO == 'X' && !occupiedTerritory && !gameOver)
     {
       playerOneMoves.push(selected);
       gameBoard.push(selected);
       getDiv.html(XorO);
+      displayStatus("Holland's Turn")
     }
-    else if (!occupiedTerritory)
+    else if (!occupiedTerritory && !gameOver)
     {
       playerTwoMoves.push(selected);
       gameBoard.push(selected);
       getDiv.html(XorO);
+      displayStatus("France's Turn")
     }
-
-    console.log(playerOneMoves);
-    console.log(occupiedTerritory);
-    winCheck(winCondition, playerOneMoves);
+    winCheck(winCondition, playerOneMoves, "France won!");
+    winCheck(winCondition, playerTwoMoves, "Holland won!");
   }
 
-  function winCheck(winCondition, playerMoves) {
+  function winCheck(winCondition, playerMoves, message) {
     for (var i = 0; i < winCondition.length; i++){
       var winComb = winCondition[i].filter(function(value){
         return playerMoves.indexOf(value) != -1;
       })
+
       if (winComb.length == 3)
-        alert("You won!");
+        {
+          displayStatus(message);
+          gameOver = true;
+          break;
+        }
+      else if (playerMoves.length == 5)
+        {
+          displayStatus("War is hell... stalemate!");
+          gameOver = true;
+          break;
+        }
     }
   }
 
