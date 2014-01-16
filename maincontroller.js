5d88955b35c8ed56a3a868d520c40b3f8fbfc29f
Hai Nguyen <hmn91733@gmail.com>
2014-01-15 23:58:42 -0800

    Add botAI's reaction to bad/odd move

--- maincontroller.js
+++ maincontroller.js
@@ -7,6 +7,8 @@ app.controller("MainController", function($scope){
   var playerOneMoves = [];
   var playerTwoMoves = [];
   var gameBoard = [];
+  var botMoves = [];
+  var fatalBlow = [];
   var gameOver = true;
   $scope.statusBox = "Click start to play!";
   $scope.gameArray = [[0,1,2],[3,4,5],[6,7,8]];
@@ -30,6 +32,7 @@ app.controller("MainController", function($scope){
     displayStatus("France's Turn");
     playerOneMoves = [];
     playerTwoMoves = [];
+    botMoves = [];
     gameBoard = [];
     gameOver = false;
 
@@ -85,9 +88,13 @@ app.controller("MainController", function($scope){
     }
 
     if (XorO == 'X' && !occupiedTerritory && !gameOver)
+    {
       playerAction(playerOneMoves, "Holland's Turn");
-    else if (!occupiedTerritory && !gameOver)
-      playerAction(playerTwoMoves, "France's Turn");
+      botAI(); // Remove this and uncomment 96-97 to resume player vs player
+    }
+    // BOT MADNESS
+    // else if (!occupiedTerritory && !gameOver)
+    //   playerAction(playerTwoMoves, "France's Turn");
 
     winCheck(winCondition, playerOneMoves, "France won!");
     winCheck(winCondition, playerTwoMoves, "Holland won!");
@@ -113,6 +120,104 @@ app.controller("MainController", function($scope){
     }
   }
 
+  // Return true if territory is occupied
+  function occupiedTerritory(territory){
+    var getTerritory = $("#" + territory).html();
+    return isNaN(getTerritory);
+  }
+  // AI MADNESSSSS
+  // Calculate bot's move by using player, bot, and windCondition
+  function calculateBotMove(player, bot){
+    for (var i = 0; i < winCondition.length; i++)
+    {
+      var botWinningComb = bot.filter(function(value){
+        return winCondition[i].indexOf(value) != -1;
+      })
+
+      var winningMove = winCondition[i].filter(function(value){
+        return bot.indexOf(value) == -1;
+      })
+
+      if (botWinningComb.length == 2 && !occupiedTerritory(winningMove[0]))
+      {
+        fatalBlow = winningMove;
+        console.log("winning moveee");
+        break;
+      }
+    }
+
+    if (fatalBlow.length == 0)
+    {
+      for (var i = 0; i < winCondition.length; i++)
+      {
+        // Check player's current moves for a 2/3 winning comb.
+        var playerWinningComb = player.filter(function(value) {
+          return winCondition[i].indexOf(value) != -1;
+        })
+
+        if (player.length >= 2)
+        {
+          var availableTerritory = winCondition[i].filter(function(value) {
+            return player.indexOf(value) == -1;
+          })
+
+          if (availableTerritory.length > 1) { continue; }
+        }
+        // Reaction to bad/odd move
+        if (player.length == 2 && playerWinningComb.length == 2 && occupiedTerritory(availableTerritory[0]))
+        {
+          fatalBlow = occupiedTerritory(0) ? [2] : [8];
+          break;
+        }
+
+        if (playerWinningComb.length == 2 && !occupiedTerritory(availableTerritory[0]))
+        {
+          fatalBlow = availableTerritory;
+          console.log(availableTerritory);
+          break;
+        }
+      }
+    }
+  }
+
 
   // AI MADNESS
+  function botAI(){
+    // Get random number from [0,2,6,8]
+    var secondMoveArray = [0,2,6,8];
+    var randMove = secondMoveArray[Math.floor(Math.random() * secondMoveArray.length)];
+    if (gameBoard.length == 1 && playerOneMoves[0] == 4)
+    {
+      $("#" + randMove).html("O");
+      botMoves.push(randMove);
+      gameBoard.push(randMove);
+      displayStatus("France's Turn");
+      console.log(botMoves);
+    }
+    else if (gameBoard.length == 1 && playerOneMoves[0] != 4)
+    {
+      $("#4").html("O");
+      botMoves.push(4);
+      gameBoard.push(4);
+      displayStatus("France's Turn");
+      console.log(botMoves);
+    }
+
+    if (gameBoard.length >= 3)
+    {
+      calculateBotMove(playerOneMoves, botMoves);
+      if (fatalBlow.length == 1 && !isNaN(fatalBlow))
+      {
+        $("#" + fatalBlow[0]).html("O");
+        botMoves.push(fatalBlow[0]);
+        gameBoard.push(fatalBlow[0]);
+        displayStatus("France's Turn");
+        fatalBlow = [];
+        console.log(botMoves);
+      }
+      else
+        console.log("Blah");
+    }
+    winCheck(winCondition, botMoves, "Holland won!");
+  }
 });
